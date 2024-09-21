d3848a58ee269ced1b093b399b95d1984646f701
aisosah22 <149622103+aisosah22@users.noreply.github.com>
2024-09-21 13:18:22 -0600

    Update Program.cs

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,44 +2,15 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.Write("What is your grade percentage? ");
-        string answer = Console.ReadLine();
-        int percent = int.Parse(answer);
-
-        string letter = "";
-
-        if (percent >= 90)
-        {
-            letter = "A";
-        }
-        else if (percent >= 80)
-        {
-            letter = "B";
-        }
-        else if (percent >= 70)
-        {
-            letter = "C";
-        }
-        else if (percent >= 60)
-        {
-            letter = "D";
-        }
-        else
-        {
-            letter = "F";
-        }
-
-        Console.WriteLine($"Your grade is: {letter}");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
         
-        if (percent >= 70)
-        {
-            Console.WriteLine("You passed!");
-        }
-        else
-        {
-            Console.WriteLine("Better luck next time!");
-        }
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
+
